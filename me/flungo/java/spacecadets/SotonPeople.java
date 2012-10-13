1171dfd50551faa8af91a1b6cd8bff03414f02bf
Fabrizio <fab@lungo.co.uk>
2012-10-14 00:23:31 +0100

    Just uses JSoup

--- me/flungo/java/spacecadets/SotonPeople.java
+++ me/flungo/java/spacecadets/SotonPeople.java
@@ -4,23 +4,10 @@
  */
 package me.flungo.java.spacecadets;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.w3c.dom.Node;
 
 /**
  *
@@ -33,15 +20,9 @@ public class SotonPeople {
 	 */
 	public static void main(String[] args) {
 		// TODO code application logic here
-		URL url = null;
+		Document page = null;
 		try {
-			url = new URL("http://www.ecs.soton.ac.uk/people/dem");
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		String page = null;
-		try {
-			page = readPage(url);
+			page = Jsoup.connect("http://www.ecs.soton.ac.uk/people/dem").get();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,8 +31,7 @@ public class SotonPeople {
 		System.out.println(name);
 	}
 	
-	private static String getName(String in) {
-		Document document = Jsoup.parse(in);
+	private static String getName(Document document) {
 		Elements h2s = document.getElementsByTag("h2");
 		String h2Text = null;
 		int i = 0;
@@ -61,37 +41,5 @@ public class SotonPeople {
 		}
 		
 		return h2Text;
-	}
-	
-	private static String readPage(URL url) throws Exception {
-
-        DefaultHttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url.toURI());
-        HttpResponse response = client.execute(request);
-
-        Reader reader = null;
-        try {
-            reader = new InputStreamReader(response.getEntity().getContent());
-
-            StringBuilder sb = new StringBuilder();
-            {
-                int read;
-                char[] cbuf = new char[1024];
-                while ((read = reader.read(cbuf)) != -1) {
-                    sb.append(cbuf, 0, read);
-				}
-            }
-
-            return sb.toString();
-
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
 	}
 }
